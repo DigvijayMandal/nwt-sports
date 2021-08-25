bdc699cc228e208c4219c45bbc7cc2a076d9aa81
Digvijay Mandal <60837710+DigvijayMandal@users.noreply.github.com>
2021-08-25 12:02:44 +0530

    update index

--- index.php
+++ index.php
@@ -5,133 +5,82 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NWT Sports</title>
-    <!---Font--->
-    <link rel="stylesheet" href="./resources/font.css">
+    <link rel="shortcut icon" href="./assets/logo.jpg">
 
     <!--font awsome cdn-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 
-    <!--Normalize Css-->
-    <link rel="stylesheet" href="./resources/normalize.css">
-
-    <!--Owl Carasoul-->
-    <link rel="stylesheet" href="./resources/OwlCarousel2-2.3.4/dist/assets/owl.carousel.css">
-    <link rel="stylesheet" href="./resources/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.css">
-
-    <!--Logo-->
-    <link rel="shortcut icon" href="./assets/logo.jpg">
-
-    <!--Custom css-->
     <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/contact.css">
+
 </head>
 <body>
-    <!--Header Start-->
-    <header class="header" id="intro">
-        <nav class="navbar">
-            <div class="container">
-                <div class="brand-and-toggler">
-                    <img src="./assets/logo.jpg" class="logo">
-                    <a href="./index.php" class="navbar-brand"> NWT Sports</a>
-                    <button type="button" class="navbar-toggler" id="navbar-toggler"><i class="fas fa-bars"></i></button>
+    <section class="popup" id="blur">
+        <div class="header">
+            <div class="navbar">
+                <div class="title">
+                    <img src="./assets/logo.jpg" alt="">
+                    <h2>NWT Sports</h2>
                 </div>
-                <div class="navbar-collapse">
-                    <ul class="navbar-nav">
-
-                        <li class="nav-item">
-                            <a href="#intro" class="nav-link">home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#contact-us" class="nav-link">contact us</a>
-                        </li>
-
-                    </ul>
-                </div>
+                <a href="#">Home</a>
             </div>
-        </nav>
 
-        <div class="hero-div center container">
-
-            <div class="logo-img">
-                <img src="./assets/logo.jpg" alt="">
-            </div>
-            <div class="hero-btns">
-                <a href="#our-partners"><button type="button" class="btn-trans">learn more</button></a>
-                <a href="#contact-us"><button type="button" class="btn-white">contact us</button></a>
+            <div class="content-cen">
+                <div class="brand"><img src="./assets/logo.jpg" alt=""></div>
+                <button id="open" onclick="toggle()">Contact Us</button>
             </div>
         </div>
-        
-    </header>
-    <!--End of Header-->
-    
-
-    <section class="contact" id="contact-us">
+        <div class="footer">
+            <div class="container">
+                <p class="text">Copyright &copy; | All Rights Reserved</p>
+                <div class="footer-links">
+                    <a href="#" class="center">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" class="center">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" class="center">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="center">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="#" class="center">
+                        <i class="fab fa-pinterest"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="contact" id="come">
         <div class="container">
             <div class="title">
                 <h2>Contact Us</h2>
             </div>
             <div class="row">
                 <div class="contact-left">
-                    <h2>Send Message here</h2>
                     <form action="">
                         <input type="text" class="form-control" placeholder="Name">
                         <input type="email" class="form-control" placeholder="Email">
                         <textarea placeholder="Message" rows="6"></textarea>
-                        <button type="submit" class="submit-btn">Send Now</button>
+                        <button type="submit" class="submit-btn" id="close" onclick="toggle()">Send Now</button>
                     </form>
                 </div>
-                <div class="contact-right">
-                    <div>
-                        <h2>Visit office</h2>
-                        <p class="text">
-                            Suite 4, Level 6 
- Berkeley Square House 
- Berkeley Square, Mayfair 
- W1J 6BY, UK
-                        </p>
-                    </div>
-                    <div>
-                        <h2>Send Email</h2>
-                        <p class="text">
-                            mj@nwt.realestate
-                        </p>
-                    </div>
-                </div>
             </div>
         </div>
-    </section>
-
-    <!--Footer-->
-    <footer class="footer center">
-        <div class="container">
-            <p class="text">Copyright &copy; | All Rights Reserved</p>
-            <div class="footer-links">
-                <a href="#" class="center">
-                    <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="#" class="center">
-                    <i class="fab fa-instagram"></i>
-                </a>
-                <a href="#" class="center">
-                    <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#" class="center">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a href="#" class="center">
-                    <i class="fab fa-pinterest"></i>
-                </a>
-            </div>
         </div>
-    </footer>
-    <!--Footer End-->
 
-    <!--jquery-->
-    <script src="./resources/jquery-3.6.0.js"></script>
-
-    <!--Owl Caraousal-->
-    <script src="./resources/OwlCarousel2-2.3.4/dist/owl.carousel.js"></script>
-
-    <!--custom js-->
-    <script src="./js/script.js"></script>
+    <script type="text/javascript">
+    function toggle(){
+        var blur = document.getElementById('blur');
+        blur.classList.toggle('active');
+        var popup = document.getElementById('come');
+        popup.classList.toggle('active');
+    }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 </html>
